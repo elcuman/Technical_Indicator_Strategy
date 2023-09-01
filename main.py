0e02e00a1d1e9051ce6469b8182d346fd0579a7a
ELCUMAN <elcumanhuseyin@gmail.com>
2023-09-02 01:35:59 +0300

    Eylül

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request , redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import talib as talib
-import itertools 
+import itertools
 import plotly.graph_objects as go
 import datetime
 
@@ -11,56 +10,33 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world(name=None):
-    
     return render_template('index.html')
-    #return render_template('index.html', name=name)
-
-
-
 
 @app.route("/result/")
 @app.route('/result/<sonucdegeri>/<tarih>')
+def result_world(sonucdegeri="", tarih=0):
+    hisseismi = sonucdegeri + " hissesi"
 
-
-def result_world(sonucdegeri="",tarih=0):
-    hisseismi= sonucdegeri+" hissesi"
-    # Get the stock symbol and data range from the user
-    # symbol = input("Enter the stock symbol: ")
-    # start_date_str = input("Enter the start date (YYYY-MM-DD): ")
-
-    # Define the date format
-   
     symbol = sonucdegeri
-    # Get the date input from the form
-    start_date_str  =tarih
-    # Convert the start date to a datetime object
+    start_date_str = tarih
 
-    # Convert the start date to a datetime object
     date_format = "%Y-%m-%d"
     start_date = datetime.datetime.strptime(start_date_str, date_format).date()
-
-    # Get today's date
     end_date = datetime.date.today()
-
-    # Calculate the number of days
     delta = end_date - start_date
     initial_capital = 1000
 
-    # Retrieve data from Yahoo Finance
     df = yf.download(symbol, start=start_date, end=end_date)
 
-    # Check if the "Close" column containing the closing prices exists
     if "Close" not in df.columns:
         raise ValueError("The dataframe does not contain the 'Close' column.")
 
-    # Convert the dataframe to the appropriate format for calculating technical indicators
     close = df["Close"].values.astype(float)
     high = df["High"].values.astype(float)
     low = df["Low"].values.astype(float)
     open_ = df["Open"].values.astype(float)
     volume = df["Volume"].values.astype(float)
 
-    # Function to generate buy-sell strategies
     def generate_strategies(df):
         strategies = []
         indicators = ["MACD", "RSI", "SMA9", "SMA21", "SMA50", "BollingerBands", "StochasticOscillator", "ADX", "OBV"]
@@ -72,32 +48,40 @@ def result_world(sonucdegeri="",tarih=0):
             sell_condition = np.full_like(close, True, dtype=bool)
             for indicator in strategy:
                 if indicator == "MACD":
-                    macd, _, _ = talib.MACD(close)
+                    macd = (close - np.mean(close)) / np.std(close)
                     buy_condition = buy_condition & (macd > 0)
                     sell_condition = sell_condition & (macd < 0)
                 elif indicator == "RSI":
-                    rsi = talib.RSI(close)
+                    rsi = 100 - (100 / (1 + (np.mean(close[:14]) / np.mean(close[14:]) if np.mean(close[14:]) != 0 else 1)))
                     buy_condition = buy_condition & (rsi < 35)
                     sell_condition = sell_condition & (rsi > 65)
                 elif indicator.startswith("SMA"):
                     period = int(indicator[3:])
-                    sma = talib.SMA(close, timeperiod=period)
+                    sma = np.mean(close[-period:])
                     buy_condition = buy_condition & (close > sma)
                     sell_condition = sell_condition & (close < sma)
                 elif indicator == "BollingerBands":
-                    upper, middle, lower = talib.BBANDS(close)
+                    sma = np.mean(close)
+                    std = np.std(close)
+                    upper = sma + 2 * std
+                    lower = sma - 2 * std
                     buy_condition = buy_condition & (close > lower)
                     sell_condition = sell_condition & (close < upper)
                 elif indicator == "StochasticOscillator":
-                    slowk, slowd = talib.STOCH(high, low, close)
-                    buy_condition = buy_condition & (slowk < 20) & (slowd < 20)
-                    sell_condition = sell_condition & (slowk > 80) & (slowd > 80)
+                    k_period = 14
+                    d_period = 3
+                    k_values = 100 * ((close - np.min(low[-k_period:])) / (np.max(high[-k_period:]) - np.min(low[-k_period:])))
+                    d_values = np.mean(k_values[-d_period:])
+                    buy_condition = buy_condition & (k_values[-1] < 20) & (d_values < 20)
+                    sell_condition = sell_condition & (k_values[-1] > 80) & (d_values > 80)
                 elif indicator == "ADX":
-                    adx = talib.ADX(high, low, close)
-                    buy_condition = buy_condition & (adx > 25)
-                    sell_condition = sell_condition & (adx < 20)
+                    adx_period = 14
+                    dx = 100 * np.mean(np.abs((np.mean(high[-adx_period:]) - np.mean(low[-adx_period:])) / (np.mean(high[-adx_period:]) + np.mean(low[-adx_period:]))))
+                    buy_condition = buy_condition & (dx > 25)
+                    sell_condition = sell_condition & (dx < 20)
                 elif indicator == "OBV":
-                    obv = talib.OBV(close, volume)
+                    obv_values = np.where(close > close[-1], volume, -volume)
+                    obv = np.cumsum(obv_values)
                     buy_condition = buy_condition & (obv > 0)
                     sell_condition = sell_condition & (obv < 0)
             strategies.append({
@@ -107,7 +91,6 @@ def result_world(sonucdegeri="",tarih=0):
             })
         return strategies
 
-    # Function to calculate returns for the strategies
     def calculate_returns(df, strategy, initial_capital):
         returns = []
         buy_prices = []
@@ -121,7 +104,7 @@ def result_world(sonucdegeri="",tarih=0):
                     position = 1
                     buy_prices.append(close[i])
                     positions.append(position)
-                    capital=capital*0.998
+                    capital = capital * 0.998
             elif strategy["sell_condition"][i]:
                 if position == 1:
                     position = 0
@@ -129,21 +112,19 @@ def result_world(sonucdegeri="",tarih=0):
                     positions.append(position)
                     return_pct = (sell_prices[-1] - buy_prices[-1]) / buy_prices[-1]
                     capital *= (1 + return_pct)
-                    capital=capital*0.998
+                    capital = capital * 0.998
                     returns.append(return_pct)
         if len(buy_prices) > len(sell_prices):
             sell_prices.append(close[-1])
             positions.append(0)
             return_pct = (sell_prices[-1] - buy_prices[-1]) / buy_prices[-1]
             capital *= (1 + return_pct)
-            capital=capital*0.998
+            capital = capital * 0.998
             returns.append(return_pct)
         return returns, positions, capital
 
-    # Generate strategies
     strategies = generate_strategies(df)
 
-    # Add the buy-and-hold strategy to the list of strategies
     strategies.append({
         "strategy": ("Buy and Hold",),
         "buy_condition": np.full_like(close, True, dtype=bool),
@@ -157,26 +138,20 @@ def result_world(sonucdegeri="",tarih=0):
         returns.append(sum(strategy_returns))
         capitals.append(strategy_capital)
 
-    # Find the strategy with the highest return
     max_return_index = np.argmax(returns)
     best_strategy = strategies[max_return_index]
     best_returns, best_positions, best_capital = calculate_returns(df, best_strategy, initial_capital)
 
-    # Determine the top 5 strategies with the highest returns
     top_5_strategies = sorted(strategies, key=lambda x: sum(calculate_returns(df, x, initial_capital)[0]), reverse=True)[:5]
 
-    # Get the names and returns of the top 5 strategies
-    strategy_names = [f"Strategy {i+1}: {', '.join(strategy['strategy'])}" for i, strategy in enumerate(top_5_strategies)]
+    strategy_names = [f"Strategy {i + 1}: {', '.join(strategy['strategy'])}" for i, strategy in enumerate(top_5_strategies)]
     strategy_returns = [sum(calculate_returns(df, strategy, initial_capital)[0]) for strategy in top_5_strategies]
 
-    # Find the index of the best strategy
     best_strategy_index = np.argmax(strategy_returns)
     best_strategy = top_5_strategies[best_strategy_index]
     best_strategy_name = f"Strategy {best_strategy_index + 1}: {', '.join(best_strategy['strategy'])}"
-    best_strategy2=best_strategy['strategy']
+    best_strategy2 = best_strategy['strategy']
 
-
-    # Determine today's strategy
     if best_strategy["buy_condition"][-1] == True:
         today_strategy = "Buy"
     elif best_strategy["sell_condition"][-1] == True:
@@ -184,47 +159,25 @@ def result_world(sonucdegeri="",tarih=0):
     else:
         today_strategy = "Mevcut Pozisyonu Koru"
 
-    # If the best strategy is the buy-and-hold strategy, set today's strategy to "Hold"
     if best_strategy["strategy"] == ("Buy and Hold",):
         today_strategy = "Hold"
 
-    # If the best strategy is the buy-and-hold strategy, set today's strategy to "Hold"
     if best_strategy["strategy"] == ("Buy and Hold",):
         today_strategy = "Hold"
 
-    # Show the returns and today's strategy
-    import plotly.graph_objects as go
-
-    # Determine the top 5 strategies with the highest returns
     top_5_strategies = sorted(strategies, key=lambda x: sum(calculate_returns(df, x, initial_capital)[0]), reverse=True)[:5]
 
-    # Get the names and returns of the top 5 strategies
-    strategy_names = [f"Strategy {i+1}: {strategy['strategy']}" for i, strategy in enumerate(top_5_strategies)]
+    strategy_names = [f"Strategy {i + 1}: {strategy['strategy']}" for i, strategy in enumerate(top_5_strategies)]
     strategy_returns = [sum(calculate_returns(df, strategy, initial_capital)[0]) for strategy in top_5_strategies]
 
-    # Create a visualization
     fig = go.Figure(data=[go.Bar(x=strategy_names, y=strategy_returns)])
     fig.update_layout(title='Top 5 Strategies and Returns',
-                    xaxis_title='Strategies',
-                    yaxis_title='Return Rate')
-    #fig.show()
-    print("\nStrategy with the Highest Return out of 512 Strategies:")
-    print(f"Strategy: {best_strategy['strategy']} - Return Rate: {sum(best_returns):.4f} - \nInitial Capital of {initial_capital} Turkish Liras has become {best_capital:.4f} Turkish Liras in {delta.days} days since {start_date_str}")
-    print("\nToday's Strategy:",{best_strategy['strategy']})
-    print(f"{today_strategy}")
+                      xaxis_title='Strategies',
+                      yaxis_title='Return Rate')
 
-    # Pass the data to the 'result.html' template
     return render_template('result.html', sonuc=hisseismi, today=today_strategy,
-                        initial_capital=initial_capital,
-                       start_date=start_date_str, final_capital=best_capital,best_strategy_name=best_strategy2)
-
-
-
+                           initial_capital=initial_capital,
+                           start_date=start_date_str, final_capital=best_capital, best_strategy_name=best_strategy2)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-
-
-
-
+    app.run(host="localhost", port=8000, debug=True)
